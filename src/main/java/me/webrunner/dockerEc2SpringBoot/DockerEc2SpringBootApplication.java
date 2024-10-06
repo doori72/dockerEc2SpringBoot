c0c9ceff4dc05d8e0ea926f29990304955592f8b
doori72 <doori72@gmail.com>
2024-10-06 17:25:14 +0900

    feat: project build

---
+++ src/main/java/me/webrunner/dockerEc2SpringBoot/DockerEc2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.webrunner.dockerEc2SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEc2SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerEc2SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/webrunner/dockerEc2SpringBoot/DockerEc2SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.webrunner.dockerEc2SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerEc2SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
